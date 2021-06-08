2da425ead407ce1d7f4315dc95d8fefe6ebbbc41
nurfajarismail <62985196+nurfajarismail@users.noreply.github.com>
2021-06-08 15:55:55 +0700

    git init

---
+++ android/app/src/main/kotlin/com/example/portoweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portoweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          child: Icon(Icons.person),
+        ),
+      ),
+    );
+  }
+}
